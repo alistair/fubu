4f99ac9b5c2a9a7398850fb69631a6c61f101bb9
jeremydmiller <jeremydmiller@yahoo.com>
2013-04-08 11:32:54 -0500

    added FileMatcher, converted to ripple2 along the way

---
+++ src/Fubu/Running/FileMatcher.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using FubuCore.Util;
+using System.Linq;
+
+namespace Fubu.Running
+{
+    public class FileMatcher
+    {
+        private readonly Cache<FileChangeCategory, IList<IFileMatch>> _matchers = new Cache<FileChangeCategory, IList<IFileMatch>>(x => new List<IFileMatch>());
+        private readonly Cache<string, FileChangeCategory> _results;
+
+        public FileMatcher()
+        {
+            _results = new Cache<string, FileChangeCategory>(file => {
+                if (matches(FileChangeCategory.AppDomain, file)) return FileChangeCategory.AppDomain;
+                if (matches(FileChangeCategory.Application, file)) return FileChangeCategory.Application;
+                if (matches(FileChangeCategory.Content, file)) return FileChangeCategory.Content;
+                return FileChangeCategory.Nothing;
+            });
+
+            Add(new BinFileMatch());
+            Add(new ExactFileMatch(FileChangeCategory.AppDomain, "web.config"));
+            Add(new ExtensionMatch(FileChangeCategory.AppDomain, "*.exe"));
+            Add(new ExtensionMatch(FileChangeCategory.AppDomain, "*.dll"));
+        }
+
+        public FileChangeCategory CategoryFor(string file)
+        {
+            return _results[file];
+        }
+
+        private bool matches(FileChangeCategory category, string file)
+        {
+            var matchers = _matchers[category];
+            return matchers.Any(x => x.Matches(file));
+        }
+
+        public void Add(IFileMatch match)
+        {
+            _matchers[match.Category].Add(match);
+        }
+    }
+}
---
+++ src/fubu.Testing/Running/FileMatcherTester.cs
@@ -0,0 +1,67 @@
+﻿using Fubu.Running;
+using NUnit.Framework;
+using FubuTestingSupport;
+
+namespace fubu.Testing.Running
+{
+    [TestFixture]
+    public class FileMatcherTester
+    {
+        private FileMatcher theMatcher;
+
+        [SetUp]
+        public void SetUp()
+        {
+            theMatcher = new FileMatcher();
+
+            theMatcher.Add(new EndsWithPatternMatch(FileChangeCategory.Application, "*.asset.config"));
+            theMatcher.Add(new ExtensionMatch(FileChangeCategory.Content, "*.css"));
+            theMatcher.Add(new ExtensionMatch(FileChangeCategory.Content, "*.spark"));
+
+        }
+
+        [Test]
+        public void always_return_app_domain_for_files_in_appdomain()
+        {
+            theMatcher.CategoryFor("bin\\foo").ShouldEqual(FileChangeCategory.AppDomain);
+            theMatcher.CategoryFor("bin\\innocuous.txt").ShouldEqual(FileChangeCategory.AppDomain);
+        }
+
+        [Test]
+        public void can_return_application()
+        {
+            theMatcher.CategoryFor("diagnostics.asset.config").ShouldEqual(FileChangeCategory.Application);
+        }
+
+        [Test]
+        public void web_config_is_app_domain()
+        {
+            theMatcher.CategoryFor("web.config").ShouldEqual(FileChangeCategory.AppDomain);
+        }
+
+        [Test]
+        public void dll_is_app_domain()
+        {
+            theMatcher.CategoryFor("something.dll").ShouldEqual(FileChangeCategory.AppDomain);
+        }
+
+        [Test]
+        public void exe_is_app_domain()
+        {
+            theMatcher.CategoryFor("something.exe").ShouldEqual(FileChangeCategory.AppDomain);
+        }
+
+        [Test]
+        public void match_on_extension()
+        {
+            theMatcher.CategoryFor("something.spark").ShouldEqual(FileChangeCategory.Content);
+            theMatcher.CategoryFor("something.css").ShouldEqual(FileChangeCategory.Content);
+        }
+
+        [Test]
+        public void match_on_nothing_is_nothing()
+        {
+            theMatcher.CategoryFor("foo.txt").ShouldEqual(FileChangeCategory.Nothing);
+        }
+    }
+}
