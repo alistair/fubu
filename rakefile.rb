47aa1bb61f1a1e61fedb79ec20bb5fd0cb339d9c
jeremydmiller <jeremydmiller@yahoo.com>
2013-06-02 10:30:31 -0500

    converted to ripple2, fuburake, and seeded docs.  Closes GH-10

--- rakefile.rb
+++ rakefile.rb
@@ -1,139 +1,52 @@
-COMPILE_TARGET = ENV['config'].nil? ? "Debug" : ENV['config'] # Keep this in sync w/ VS settings since Mono is case-sensitive
-CLR_TOOLS_VERSION = "v4.0.30319"
-
-buildsupportfiles = Dir["#{File.dirname(__FILE__)}/buildsupport/*.rb"]
-
-require 'rubygems/package_task'
-
-if( ! buildsupportfiles.any? )
-  # no buildsupport, let's go get it for them.
-  sh 'git submodule update --init' unless buildsupportfiles.any?
-  buildsupportfiles = Dir["#{File.dirname(__FILE__)}/buildsupport/*.rb"]
+begin
+  require 'bundler/setup'
+  require 'fuburake'
+rescue LoadError
+  puts 'Bundler and all the gems need to be installed prior to running this rake script. Installing...'
+  system("gem install bundler --source http://rubygems.org")
+  sh 'bundle install'
+  system("bundle exec rake", *ARGV)
+  exit 0
 end
 
-# nope, we still don't have buildsupport. Something went wrong.
-raise "Run `git submodule update --init` to populate your buildsupport folder." unless buildsupportfiles.any?
 
-buildsupportfiles.each { |ext| load ext }
-
-include FileTest
-require 'albacore'
-load "VERSION.txt"
-
-RESULTS_DIR = "results"
-PRODUCT = "fubu"
-COPYRIGHT = 'Copyright 2012 fubu. All rights reserved.';
-COMMON_ASSEMBLY_INFO = 'src/CommonAssemblyInfo.cs';
-BUILD_DIR = File.expand_path("build")
-ARTIFACTS = File.expand_path("artifacts")
-
-tc_build_number = ENV["BUILD_NUMBER"]
-build_revision = tc_build_number || Time.new.strftime('5%H%M')
-BUILD_NUMBER = "#{BUILD_VERSION}.#{build_revision}"
-
-props = { :stage => BUILD_DIR, :artifacts => ARTIFACTS }
-
-desc "**Default**, compiles and runs tests"
-task :default => [:compile, :unit_test]
-
-desc "Target used for the CI server"
-task :ci => [:update_all_dependencies, :default, :history, :package]
-
-desc "Update the version information for the build"
-assemblyinfo :version do |asm|
-  asm_version = BUILD_VERSION + ".0"
-  
-  begin
-    commit = `git log -1 --pretty=format:%H`
-  rescue
-    commit = "git unavailable"
-  end
-  puts "##teamcity[buildNumber '#{BUILD_NUMBER}']" unless tc_build_number.nil?
-  puts "Version: #{BUILD_NUMBER}" if tc_build_number.nil?
-  asm.trademark = commit
-  asm.product_name = PRODUCT
-  asm.description = BUILD_NUMBER
-  asm.version = asm_version
-  asm.file_version = BUILD_NUMBER
-  asm.custom_attributes :AssemblyInformationalVersion => asm_version
-  asm.copyright = COPYRIGHT
-  asm.output_file = COMMON_ASSEMBLY_INFO
-end
-
-desc "Prepares the working directory for a new build"
-task :clean => [:update_buildsupport] do
+@solution = FubuRake::Solution.new do |sln|
+	sln.compile = {
+		:solutionfile => 'src/fubu.sln'
+	}
+				 
+	sln.assembly_info = {
+		:product_name => "fubu",
+		:copyright => 'Copyright 2010-2013 Jeremy D. Miller, Josh Arnold, et al. All rights reserved.'
+	}
 	
-	FileUtils.rm_rf props[:stage]
-    # work around nasty latency issue where folder still exists for a short while after it is removed
-    waitfor { !exists?(props[:stage]) }
-	Dir.mkdir props[:stage]
-    
-	Dir.mkdir props[:artifacts] unless exists?(props[:artifacts])
-end
-
-def waitfor(&block)
-  checks = 0
-  until block.call || checks >10 
-    sleep 0.5
-    checks += 1
-  end
-  raise 'waitfor timeout expired' if checks > 10
-end
-
-desc "Compiles the app"
-task :compile => [:restore_if_missing, :clean, :version] do
-  MSBuildRunner.compile :compilemode => COMPILE_TARGET, :solutionfile => 'src/fubu.sln', :clrversion => CLR_TOOLS_VERSION
-
-  target = COMPILE_TARGET.downcase
-end
-
-def copyOutputFiles(fromDir, filePattern, outDir)
-  Dir.glob(File.join(fromDir, filePattern)){|file| 		
-	copy(file, outDir, :preserve => true) if File.file?(file)
-  } 
-end
-
-def cleanDirectory(dir)
-  FileUtils.rm_rf dir;
-  waitfor { !exists?(dir) }
-  Dir.mkdir dir
-end
-
-def cleanFile(file)
-  File.delete file unless !File.exist?(file)
-end
-
-desc "Runs unit tests"
-task :test => [:unit_test]
-
-desc "Run unit tests"
-task :unit_test do 
-  runner = NUnitRunner.new :compilemode => COMPILE_TARGET, :source => 'src', :platform => 'x86'
-  tests = Array.new
-  file = File.new("TESTS.txt", "r")
-  assemblies = file.readlines()
-  assemblies.each do |a|
-	test = a.gsub("\r\n", "").gsub("\n", "")
-	tests.push(test)
-  end
-  file.close
-  
-  runner.executeTests tests
-end
-
-def self.bottles(args)
-  bottles = Platform.runtime(Nuget.tool("Bottles", "BottleRunner.exe"))
-  sh "#{bottles} #{args}"
+	sln.ripple_enabled = true
+	sln.fubudocs_enabled = true
+	sln.ci_steps = [:archive_gem]
 end
 
 
-def self.fubu(args)
-  fubu = Platform.runtime("src/fubu/bin/#{COMPILE_TARGET}/fubu.exe")
-  sh "#{fubu} #{args}"
+desc "Replaces the existing installed gem with the new version for local testing"
+task :local_gem => [:create_gem] do
+	sh 'gem uninstall fubu'
+	Dir.chdir 'pkg' do
+	    sh 'gem install fubu'
+    end
+end
+
+desc "Moves the gem to the archive folder"
+task :archive_gem => [:create_gem] do
+	copyOutputFiles "pkg", "*.gem", "artifacts"
+end
+
+desc "Outputs the command line usage"
+task :dump_usages => [:compile] do
+  bottles 'dump-usages fubu src/Fubu.Docs/fubu.cli.xml'
 end
 
 desc "Creates the gem for fubu.exe"
-task :create_gem do
+task :create_gem => [:compile, :ilrepack] do
+    require "rubygems/package"
 	cleanDirectory 'bin';	
 	cleanDirectory 'pkg'
 	
@@ -143,9 +56,7 @@ task :create_gem do
 	copyOutputFiles "src/fubu/bin/#{COMPILE_TARGET}", '*.exe', 'bin'
 	
 	FileUtils.copy 'fubu', 'bin'
-	
-	Rake::Task[:gem].invoke
-end
+
 
 	spec = Gem::Specification.new do |s|
 	  s.platform    = Gem::Platform::RUBY
@@ -162,10 +73,11 @@ end
 	  s.email             = 'fubumvc-devel@googlegroups.com'
 	  s.homepage          = 'http://fubu-project.org'
 	  s.rubyforge_project = 'fubu'
-	end
+	end   
+    puts "ON THE FLY SPEC FILES"
+    puts spec.files
+    puts "=========="
 
-
-Gem::PackageTask.new(spec) do |pkg|
-  pkg.need_zip = true
-  pkg.need_tar = true
+    Gem::Package::build spec, true
 end
+
