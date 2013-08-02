0825b1bac0367b5d43e4d5b112568f27b1429c93
jeremydmiller <jeremydmiller@yahoo.com>
2013-08-02 13:27:41 -0500

    closes GH-52

--- templates/alteration/structuremap/%STRUCTUREMAP_REGISTRY%.cs
+++ templates/alteration/structuremap/%STRUCTUREMAP_REGISTRY%.cs
@@ -7,6 +7,13 @@ namespace %NAMESPACE%
 		public %STRUCTUREMAP_REGISTRY%()
 		{
 			// make any StructureMap configuration here
+			
+            // Sets up the default "IFoo is Foo" naming convention
+            // for auto-registration within this assembly
+            Scan(x => {
+                x.TheCallingAssembly();
+                x.WithDefaultConventions();
+            });
 		}
 	}
 }
